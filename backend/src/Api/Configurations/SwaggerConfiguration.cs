7cad7c358ac46096e9980c9d9c75501dbffca5ec
Viliane <vilianeso@gmail.com>
2025-01-29 12:04:42 -0300

    feature_authorizeSwagger - Inclusão chave authorize no swagger

--- backend/src/Api/Configurations/SwaggerConfiguration.cs
+++ backend/src/Api/Configurations/SwaggerConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace Api.Configurations
+﻿using Microsoft.OpenApi.Models;
+
+namespace Api.Configurations
 {
     public static class SwaggerConfiguration
     {
@@ -6,6 +8,34 @@
         {
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta forma: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    []
+                }
+            });
+            });
 
             return builder;
         }
