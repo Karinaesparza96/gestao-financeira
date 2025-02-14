768ef0cd2809f27198dc661e3356609b9d8c0f7c
karinaesparza <karinaesparza296@gmail.com>
2025-02-14 17:58:06 -0300

    adiciona chat.js/ng2charts e grafico receita vs despesas

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -4,10 +4,13 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()),  provideAnimationsAsync(),
-    provideHttpClient()
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes, withComponentInputBinding()),  
+    provideAnimationsAsync(),
+    provideHttpClient(), provideCharts(withDefaultRegisterables())
   ]
 };
